f23d4f8b90e21653603853bd8adb8c262a5560da
Lovro <lovroperaic@gmail.com>
2025-09-19 14:54:29 +0200

    prevent default behaviour on pointerdown on carousel

--- static/script.js
+++ static/script.js
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     carousel.addEventListener("pointerdown", e => {
+        e.preventDefault();
         touchStartX = e.clientX;
     });
     carousel.addEventListener("pointerup", e => {
