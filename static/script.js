1f3b76ead84f88a468164dd117d9d425f9a65bd3
Lovro <lovroperaic@gmail.com>
2025-09-19 14:51:04 +0200

    add swiping to carousel

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 const accessStyle = getComputedStyle(document.documentElement);
+const carousel = document.querySelector(".carousel");
 const carouselPositionVar = "--carousel-position";
 const carouselControlLeft = document.querySelector(".carousel-control#left");
 const carouselControlRight = document.querySelector(".carousel-control#right")
@@ -6,6 +7,9 @@ const carouselLength = document.querySelector(".carousel").children.length;
 const carouselTransitionVar = "--carousel-transition-time";
 const carouselTransitionTime = accessStyle.getPropertyValue("--carousel-transition-time-setting");
 const setStyle = document.documentElement.style;
+
+let touchStartX = 0;
+let touchEndX = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
     toggleCarouselControls(Number(accessStyle.getPropertyValue(carouselPositionVar)));
@@ -19,22 +23,46 @@ document.addEventListener("DOMContentLoaded", () => {
     carouselControlRight.addEventListener("click", () => {
         scrollCarousel(1);
     });
+
+    carousel.addEventListener("pointerdown", e => {
+        touchStartX = e.clientX;
+    });
+    carousel.addEventListener("pointerup", e => {
+        touchEndX = e.clientX;
+        handleSwipe();
+    });
 });
 
+
+function handleSwipe() {
+    const swipeDistance = touchEndX - touchStartX;
+    const swipeThreshold = 50;
+
+    if (swipeDistance > swipeThreshold) {
+        scrollCarousel(-1);
+    } else if (swipeDistance < -swipeThreshold) {
+        scrollCarousel(1);
+    }
+}
 
 function scrollCarousel(direction) {
     let currentPosition = Number(accessStyle.getPropertyValue(carouselPositionVar));
     const carouselVisibleLength = Number(accessStyle.getPropertyValue("--carousel-visible-images"));
 
     if (currentPosition >= carouselLength - carouselVisibleLength) currentPosition = carouselLength - carouselVisibleLength;
-    if (direction !== 0) setStyle.setProperty(carouselTransitionVar, carouselTransitionTime);
+    
+    let newPosition = currentPosition + direction;
 
-    setStyle.setProperty(carouselPositionVar, currentPosition + direction);
-    toggleCarouselControls(currentPosition + direction, carouselVisibleLength);
+    if (newPosition >= 0 && newPosition <= carouselLength - carouselVisibleLength) {
+        if (direction !== 0) setStyle.setProperty(carouselTransitionVar, carouselTransitionTime);
 
-    setTimeout(() => {
-        setStyle.setProperty(carouselTransitionVar, "none");
-    }, 500);
+        setStyle.setProperty(carouselPositionVar, currentPosition + direction);
+        toggleCarouselControls(currentPosition + direction, carouselVisibleLength);
+
+        setTimeout(() => {
+            setStyle.setProperty(carouselTransitionVar, "none");
+        }, 500);
+    }
 }
 
 function toggleCarouselControls(position, visibleLength) {
